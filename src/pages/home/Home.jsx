0e2cf3487974fa2239b657203450fe8cb6cbf4cd
Arit Nath <idofarit@gmail.com>
2023-11-24 06:35:25 +0530

    homePage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./style.scss";
 import HeroBanner from "../home/heroBanner/HeroBanner";
 import Trending from "../home/trending/Trending";
+import Popular from "../home/popular/Popular";
+import TopRated from "../home/topRated/TopRated";
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
       <Trending />
+      <Popular />
+      <TopRated />
     </div>
   );
 };
